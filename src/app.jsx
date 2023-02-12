5dcc0536fd1852fec574288af468a78527b77c11
wombat24455 <wombathodge@gmail.com>
2023-02-12 22:24:49 +0000

    site broke

--- src/app.jsx
+++ src/app.jsx
@@ -2,8 +2,6 @@ import './app.css';
 import { Footer } from './fragments/footer';
 import { Intro } from './fragments/main/intro';
 import { Navbar } from './fragments/navbar';
-//import { Projects } from './fragments/projects/projects';
-//import { Background } from './fragments/background';
 
 /* Figure out Preact Portals */
 
